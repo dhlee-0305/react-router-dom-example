35f39ead2c8806de4a9309e54cd50f478825f41c
dhlee-0305 <dhlee.0305@gmail.com>
2025-02-04 14:49:18 +0900

    4장 Context API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {createContext, useContext} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './style.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes, NavLink, useParams } from 'react-router-dom';
+import styled from "styled-components";
 
-import styled from 'styled-components';
+const themeDefault = {border: '10px solid red'};
+const themeContext = createContext(themeDefault);
 
 const SimpleButton = styled.button`
   color: white;
@@ -95,8 +98,17 @@ function Contact(){
 }
 
 function App(){
+  const theme = useContext(themeContext);
+  console.log('theme:', theme);
   return (
     <div>
+      <themeContext.Provider value={{ border: "10px solid blue" }}>
+        <div className="root" style={theme}>
+          <h1>Hello World!</h1>
+          <Sub1 />
+        </div>
+      </themeContext.Provider>
+
       <SimpleButton>Simple Button</SimpleButton>
       <LargeButton>Large Button</LargeButton>
       <ReactButton>React Button</ReactButton>
@@ -122,6 +134,37 @@ function App(){
         <Route path="/contact" element={<Contact />} />
         <Route path="/*" element={"Not Found"} />
       </Routes>
+    </div>
+  );
+}
+
+function Sub1(){
+  const theme = useContext(themeContext);
+  return (
+    <themeContext.Provider value={{border: '10px solid green'}}>
+      <div style={theme}>
+        <h1>Sub1</h1>
+        <Sub2 />
+      </div>
+    </themeContext.Provider>
+  );
+}
+
+function Sub2(){
+  const theme = useContext(themeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub2</h1>
+      <Sub3 />
+    </div>
+  );
+}
+
+function Sub3(){
+  const theme = useContext(themeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub3</h1>
     </div>
   );
 }
